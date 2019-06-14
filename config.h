ac014203237df87010e0e08453d676e76cc0fc1a
amariampolskiy <43878920+amariampolskiy@users.noreply.github.com>
2019-06-14 20:40:06 +0300

    remove some tempzones and unnecesary emoji

--- config.h
+++ config.h
@@ -63,14 +63,14 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ keymap,      " ⌨ %s",      ""},
-	{ run_command, " 🗓 %s ",     "date +'%a %b %d'"},
-	{ run_command, "⌚ %s",      "date +%T"},
-	{ cpu_perc,    ";💻 %s%%",   "" },
-	{ temp,        " 🌡 %s",      "/sys/class/thermal/thermal_zone0/temp" },
-	{ temp,        "/%s",        "/sys/class/thermal/thermal_zone1/temp" },
-	{ temp,        "/%s",        "/sys/class/thermal/thermal_zone2/temp" },
-	{ temp,        "/%s C°",     "/sys/class/thermal/thermal_zone3/temp" },
-	{ disk_perc,   " 💾: %s%%",  "/"},
-	{ ram_perc,    " RAM: %s%%", ""},
+	{ keymap,      " ⌨ %s",        ""},
+	{ run_command, " 🗓 %s ",       "date +'%a %b %d'"},
+	{ run_command, "⌚ %s",         "date +%T"},
+	{ cpu_perc,    ";💻 cpu %s%%",  "" },
+	{ temp,        " 🌡 %s",        "/sys/class/thermal/thermal_zone0/temp" },
+	{ temp,        "/%s",           "/sys/class/thermal/thermal_zone1/temp" },
+/*	{ temp,        "/%s",           "/sys/class/thermal/thermal_zone2/temp" }, */
+/*	{ temp,        "/%s C°",        "/sys/class/thermal/thermal_zone3/temp" }, */
+	{ disk_perc,   " disk %s%%",    "/"},
+	{ ram_perc,    " mem %s%%",     ""},
 };
